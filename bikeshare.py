80cca5dd5bba7da29e30a0264091cfe0291e01c6
Noaman Baiyat <nbaiyat@gmail.com>
2021-02-24 15:52:06 -0600

    commented first portion of code which is optimized

--- bikeshare.py
+++ bikeshare.py
@@ -37,7 +37,7 @@ def choice(prompt, choices=('y', 'n')):
                   "be sure to enter a valid option:\n>")
 
     return choice
-
+"code is optimized for the first portion"
 
 def get_filters():
     """Ask user to specify city(ies) and filters, month(s) and weekday(s).
